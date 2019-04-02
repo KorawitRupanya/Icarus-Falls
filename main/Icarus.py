481a305ef0f62f623dcaf41aba4431d7ada94725
KorawitRupanya <smart1micky268@gmail.com>
2019-04-02 21:27:42 +0700

    change back ground color

--- main/Icarus.py
+++ main/Icarus.py
@@ -28,7 +28,7 @@ class Window(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.BLACK)
 
         self.start()
 
