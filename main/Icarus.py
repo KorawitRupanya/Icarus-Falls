c93bb6f663a8daa2a9219b88c7d0bebb95ba45f6
KorawitRupanya <smart1micky268@gmail.com>
2019-05-03 14:34:38 +0700

    add sound effect add fire and fix a lot of bugs

--- main/Icarus.py
+++ main/Icarus.py
@@ -1,5 +1,7 @@
 import arcade
+import pygame
 import subprocess
+import os
 from models import World, Player, Arrow, Fire
 
 SCREEN_WIDTH = 600
@@ -79,13 +81,18 @@ class Window(arcade.Window):
         self.start()
         self.offset = 0
         self.arrows = []
+        self.fires = []
+        self.numArrow = 5
         self.background = arcade.load_texture(
             ".././images/Back ground/sky.jpg")
-        for i in range(self.world.arrowNumbers):
+
+        for i in range(self.numArrow):
             self.arrows.append(ArrowSprite(model=self.world.arrow[i]))
         self.arrow_sprite = self.arrows
-        # for i in range(n):
-        # self.fire_sprite = [FireSprite(model=self.world.fire[n])]
+
+        for n in range(self.world.fireNumbers):
+            self.fires.append(FireSprite(model=self.world.fire[n]))
+        self.fire_sprite = self.fires
 
     def start(self):
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -105,6 +112,10 @@ class Window(arcade.Window):
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, (SCREEN_HEIGHT // 2 + self.offset)-SCREEN_HEIGHT,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 
+    def draw_lp(self):
+        arcade.draw_rectangle_filled(
+            (self.width - 120) + (100 - self.world.player.lp), 30, self.world.player.lp * 2, 20, arcade.color.CHERRY)
+
     def update(self, delta):
         self.world.update(delta)
         if self.world.is_dead():
@@ -117,14 +128,23 @@ class Window(arcade.Window):
         arcade.start_render()
 
         self.draw_background()
+
         for arrow in self.arrow_sprite:
             arrow.draw()
-        arcade.draw_text(str(self.world.player.lp),
-                         self.width-570, self.height-30, arcade.color.BLACK, 20)
+
+        for fire in self.fire_sprite:
+            fire.draw()
+
         arcade.draw_text(str(int(self.world.score)),
-                         self.width - 30, self.height - 30,
-                         arcade.color.BLACK, 10)
+                         25, self.height - 40,
+                         arcade.color.BLACK, 20)
+
+        arcade.draw_text("LP",
+                         self.width - 35, 45, arcade.color.BLACK, 10)
+
         self.player_sprite.draw()
+
+        self.draw_lp()
 
 
 def main():
--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from random import randint
 from detect import is_hit
 import arcade.key
+import math
+import os
 
 DIR_STILL = 0
 DIR_RIGHT = 1
@@ -43,43 +45,56 @@ class Player:
         if self.y+35 >= self.world.height:
             self.vy *= -1
         self.vy -= Player.GRAVITY
-        if(self.y - 50 < 0):
+        if self.y - 50 < 0:
             self.world.freeze()
+        # if self.lp == 0:
+        #     self.cannotmove()
+        # else:
         self.direction = self.next_direction
         self.move(self.direction)
+
+    def cannotmove(self):
+        MOVEMENT_SPEED = 0
 
     def move(self, direction):
         self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
         self.y += MOVEMENT_SPEED * DIR_OFFSETS[direction][1]
 
 
-class Soil:
+class Fire:
+    FIRE_SPEED = 2
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
-        self.vx = 0
-        self.vy = 0
+        self.plus_speed = 1
+        self.change_x = 0
+        self.change_y = 0
 
+    def update(self, delta):
 
-class Fire:
-    def __init__(self, world, x, y, vx, vy):
-        self.world = world
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
+        self.x += self.change_x
+        self.y += self.change_y
 
-    def move(self):
-        self.x += self.vx
-        self.y += self.vy
+        if randint(0, 100) == 0:
+            start_x = self.x
+            start_y = self.y
 
-        if self.x > self.world.width - 60  \
-                or self.x < 60:
-            self.vx *= -1
-        if self.y > self.world.height - 60 \
-                or self.y < 60:
-            self.vy *= -1
+            dest_x = self.world.player.x
+            dest_y = self.world.player.y
+
+            # Do math to calculate how to get the bullet to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the bullet will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            # Taking into account the angle, calculate our change_x
+            # and change_y. Velocity is how fast the bullet travels.
+            self.change_x = math.cos(angle) * Fire.FIRE_SPEED
+            self.change_y = math.sin(angle) * Fire.FIRE_SPEED
 
 
 class Arrow:
@@ -89,7 +104,7 @@ class Arrow:
         self.world = world
         self.x = randint(0, 600)
         self.y = y
-        self.vy = 1
+        self.vy = 0.1
 
     def up_speed(self):
         if(self.world.score % 100 == 0):
@@ -125,8 +140,10 @@ class World:
         self.width = width
         self.height = height
         self.arrowNumbers = 5
+        self.fireNumbers = 2
         self.arrows = []
         self.player = Player(self, width // 2, height // 2)
+        self.fires = []
         self.state = World.STATE_FROZEN
         self.score = 0
         for i in range(self.arrowNumbers):
@@ -135,11 +152,22 @@ class World:
         pass
         self.arrow = self.arrows
 
+        for i in range(self.fireNumbers):
+            self.fires.append(
+                Fire(self, width//2, height//2)
+            )
+        self.fire = self.fires
+
+        sound = arcade.sound.load_sound(".././soundEffect/themeSong.wav")
+        arcade.play_sound(sound)
+
     def update(self, delta):
         if self.state in [World.STATE_FROZEN, World.STATE_DEAD]:
             return
         self.player.check = False
         self.player.update(delta)
+        for i in self.fires:
+            i.update(delta)
         for i in self.arrow:
             if i.hit(self.player):
                 self.player.check = True
